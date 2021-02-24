1dda9cdf2c363e72f5b908643509b4781430421a
anco <ancovorbish9oita180rz@gmail.com>
2021-02-24 23:35:52 +0900

    コミット漏れ

---
+++ src/main/kotlin/com/hatenablog/ancozerticht/imassc/domain/service/SupportSkillListCombiner.kt
@@ -0,0 +1,20 @@
+package com.hatenablog.ancozerticht.imassc.domain.service
+
+import com.hatenablog.ancozerticht.imassc.domain.model.SupportSkill
+import com.hatenablog.ancozerticht.imassc.domain.model.SupportSkillGeneral
+import com.hatenablog.ancozerticht.imassc.domain.model.SupportSkillOther
+
+class SupportSkillListCombiner {
+    fun combine(generalList: List<SupportSkillGeneral>, otherList: List<SupportSkillOther>): List<SupportSkill> {
+        return generalList.map {
+            combine(it, otherList)
+        }
+    }
+
+    private fun combine(general: SupportSkillGeneral, otherList: List<SupportSkillOther>): SupportSkill {
+        val other = otherList.firstOrNull {
+            it.cardName == general.cardName
+        } ?: SupportSkillOther.defaultObject
+        return SupportSkill(general, other)
+    }
+}
